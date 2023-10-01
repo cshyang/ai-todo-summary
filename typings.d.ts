196737b9b17e571d0191f2d0230db67dfdad751d
Chau Shyang Ch'ng <cshyang@Shyangs-MacBook-Air.local>
2023-10-01 22:57:26 +0800

    fix inprogress typo

--- typings.d.ts
+++ typings.d.ts
@@ -2,7 +2,7 @@ interface Board {
   columns: Map<TypedColumn, Column>;
 }
 
-type TypedColumn = "todo" | "inprogress" | "done";
+type TypedColumn = "todo" | "in-progress" | "done";
 
 interface Column {
   id: TypedColumn;
